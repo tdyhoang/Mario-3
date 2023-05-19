d4867db4b29547e235d4444583648c99bf1abf62
tdyhoang <tdyhoang@gmail.com>
2023-05-19 21:58:26 +0700

    Change the tileset resource used Map scene's almost completed

--- src/Resources/Map1/misc.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="misc" tilewidth="17" tileheight="17" tilecount="870" columns="29">
- <image source="smb3_background_tiles.png" trans="ff80c0" width="498" height="516"/>
-</tileset>
