4a0a2b6802dc1dba61ae6e7543f8b7ee78e72fb9
tdyhoang <tdyhoang@gmail.com>
2023-06-06 15:59:08 +0700

    Add Object tags

---
+++ Mario-3/Framework/GameComponents/GameObject/ObjectTag.h
@@ -0,0 +1,35 @@
+#pragma once
+
+enum class ObjectTag
+{
+	None, // Default
+	Player,
+	PlayerController,
+	SmallPlayer,
+	Solid,
+	GhostPlatform,
+	Enemy,
+	Pipe,
+
+	MarioFireBall,
+	VenusFireBall,
+	QuestionBlock,
+	Coin,
+	Brick,
+	Gift,
+	SuperMushroom,
+	RaccoonTail,
+	Effect,
+	Boomerang,
+
+	Portal,
+	Label,
+	SwitchBlock,
+	Card,
+
+	MarioMap,
+
+	Menu,
+
+	MovingPlatform
+};
