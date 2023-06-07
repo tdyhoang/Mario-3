01469a51a8a912566adb24eb7caedcd56f5fe532
tdyhoang <tdyhoang@gmail.com>
2023-06-07 08:39:29 +0700

    Implement Small Mario

---
+++ Mario-3/Game/GameObjects/Mario/States/SmallMario.cpp
@@ -0,0 +1,55 @@
+#include "SmallMario.h"
+#include "../MarioConst.h"
+#include "../../../../Framework/GameComponents/Graphics/Animation/AnimationManager.h"
+#include "../../../../Framework/Ultis/Ultis.h"
+#include "../MarioHitBox.h"
+
+CSmallMario::CSmallMario()
+{
+	CSmallMario::Init();
+	CMario::InitProperties();
+	CSmallMario::SetState("IDLE");
+
+	tag = ObjectTag::SmallPlayer;
+	//marioStateTag = MarioStates::SmallMario;
+
+	canCrouch = false;
+	canAttack = false;
+}
+void CSmallMario::Init()
+{
+	LoadAnimation();
+	CMarioHitBox* collisionBox = new CMarioHitBox();
+	collisionBox->SetSizeBox(SMALL_MARIO_BBOX);
+	collisionBox->SetGameObjectAttach(this);
+	collisionBox->SetName("Small-Mario");
+	collisionBox->SetDistance(D3DXVECTOR2(0.0f, 0.0f));
+	SetRelativePositionOnScreen(D3DXVECTOR2(0.0f, SUPER_MARIO_BBOX.y / 2 - SMALL_MARIO_BBOX.y / 2));
+	hitBoxs->push_back(collisionBox);
+}
+
+void CSmallMario::LoadAnimation()
+{
+	CMario::LoadAnimation();
+
+	DebugOut(L"Load animation Small Mario \n");
+	auto animationManager = CAnimationManager::GetInstance();
+	AddAnimation("IDLE", animationManager->Get("ani-small-mario-idle"));
+	AddAnimation("WALK", animationManager->Get("ani-small-mario-walk"));
+	AddAnimation("RUN", animationManager->Get("ani-small-mario-run"));
+	AddAnimation("JUMP", animationManager->Get("ani-small-mario-jump"));
+	AddAnimation("FALL", animationManager->Get("ani-small-mario-jump"));
+	AddAnimation("SQUAT", animationManager->Get("ani-small-mario-idle"));
+	AddAnimation("HIGH-JUMP", animationManager->Get("ani-small-mario-high-jump"));
+	AddAnimation("PICK-UP-WALK", animationManager->Get("ani-small-mario-pick-up-walk"));
+	AddAnimation("PICK-UP-IDLE", animationManager->Get("ani-small-mario-pick-up-idle"));
+	AddAnimation("PICK-UP-JUMP", animationManager->Get("ani-small-mario-pick-up-jump"));
+	AddAnimation("KICK", animationManager->Get("ani-small-mario-kick"), false);
+	AddAnimation("IDLE-FRONT", animationManager->Get("ani-small-mario-idle-front"));
+	AddAnimation("DIE", animationManager->Get("ani-small-mario-die"));
+}
+
+CSmallMario::~CSmallMario()
+{
+	CGameObject::~CGameObject();
+}
---
+++ Mario-3/Game/GameObjects/Mario/States/SmallMario.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "../Mario.h"
+class CSmallMario : public CMario
+{
+public:
+	CSmallMario();
+	void Init() override;
+	void LoadAnimation() override;
+	~CSmallMario();
+};
