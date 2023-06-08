4a75972ef8f3f7e1d92e357d2e04e77c7e11c95f
tdyhoang <tdyhoang@gmail.com>
2023-06-08 16:58:18 +0700

    Resources Update Enlarge the texture images to match game window Of course the map is also enlarged Add some objects into the map

--- Mario-3/Resources/Maps/misc.tsx
+++ Mario-3/Resources/Maps/misc.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="misc" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1533" columns="73">
- <image source="../Textures/misc.png" trans="e0a3d8" width="1242" height="358"/>
+<tileset version="1.10" tiledversion="1.10.1" name="misc" tilewidth="48" tileheight="48" spacing="3" margin="3" tilecount="13797" columns="73">
+ <image source="../Textures/misc.png" trans="e0a3d8" width="3726" height="1074"/>
  <tile id="213">
   <animation>
    <frame tileid="213" duration="100"/>
