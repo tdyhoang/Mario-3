76552180ab4d6908a22f56f29767631fd7d715ff
tdyhoang <tdyhoang@gmail.com>
2023-06-16 14:23:57 +0700

    Add Brick Effect

---
+++ Mario-3/Game/GameObjects/Misc/Effect/BrickEffect.cpp
@@ -0,0 +1,40 @@
+#include "BrickEffect.h"
+#include "../../../../Framework/GameComponents/Graphics/Animation/AnimationManager.h"
+#include "../../../../Framework/GameComponents/Game.h"
+#include "../Misc.h"
+
+CBrickEffect::CBrickEffect()
+{
+	LoadAnimation();
+	SetState("BRICK");
+	SetScale(D3DXVECTOR2(0.70f, 0.70f));
+	physicsBody->SetGravity(DEBRIS_GRAVITY);
+	isEnabled = false;
+}
+
+void CBrickEffect::LoadAnimation()
+{
+	auto animations = CAnimationManager::GetInstance();
+	AddAnimation("BRICK", animations->Get("ani-brick-debris"));
+}
+
+void CBrickEffect::LinkToPool(CObjectPool* pool)
+{
+	this->pool = pool;
+}
+
+void CBrickEffect::Update(DWORD dt, CCamera* cam, CCamera* uiCam)
+{
+	auto deltaTime = dt * CGame::GetTimeScale();
+	auto vel = physicsBody->GetVelocity();
+
+	auto pos = GetPosition();
+	SetPosition(pos + vel * deltaTime);
+
+	vel.y += physicsBody->GetGravity() * deltaTime;
+	vel.y = (vel.y < DEBRIS_MIN_FALL) ? vel.y : DEBRIS_MIN_FALL;
+	physicsBody->SetVelocity(vel);
+
+	if (transform.position.y > startPosition.y && *physicsBody->GetVelocity() > 0)
+		isEnabled = false;
+}
---
+++ Mario-3/Game/GameObjects/Misc/Effect/BrickEffect.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "EffectObject.h"
+
+class CBrickEffect : public CEffectObject
+{
+	CObjectPool* pool;
+public:
+	CBrickEffect();
+	void LoadAnimation();
+	void LinkToPool(CObjectPool* pool);
+	void Update(DWORD dt, CCamera* cam, CCamera* uiCam) override;
+};
